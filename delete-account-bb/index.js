67e7465cfc69630a88ebb06f6c7d6202cd3967e8
Arjun <nagarjun_nagesh@outlook.com>
2019-12-30 14:56:50 +0100

    reset account confiugration

--- delete-account-bb/index.js
+++
@@ -1,7 +0,0 @@
-exports.handler =  async function(event, context) {
-    //console.log('event body - ' + event);
-    const response = {
-        message: "Success!! You invoked a delete account Lambda"
-    };
-    return response;
-};
--- reset-account-bb/index.js
+++ reset-account-bb/index.js
@@ -1,7 +1,91 @@
+const http = require('http');
 exports.handler =  async function(event, context) {
-   console.log('event body - ' + JSON.stringify(event));
-    const response = {
-            message: 'Hello from Lambda!'
-    }
-    return response;
+    // Concurrently call multiple APIs and wait for the response
+   let events = [];
+   events.push(deleteAllTransactions(event));
+   events.push(deleteAllBudget(event));
+   events.push(deleteAllAccount(event));
+   let result = await Promise.all(events);
+   return Object.assign({ result });
+}
+
+function deleteAllTransactions(event) {
+
+    return new Promise((resolve, reject) => {
+        const options = {
+            host: 'REPLACE HOST INSTANCE',
+            path: '/api/transactions/' + event['body-json'].financialPortfolioId,
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            json: true
+        };
+
+        const req = http.request(options, (res) => {
+          resolve('Success');
+        });
+
+        req.on('error', (e) => {
+          reject(e.message);
+        });
+
+        // send the request
+        req.write('');
+        req.end();
+    });
+};
+
+function deleteAllBudget(event) {
+    
+    return new Promise((resolve, reject) => {
+        const options = {
+            host: 'REPLACE HOST INSTANCE',
+            path: '/api/transactions/?financialPortfolioId=' + event['body-json'].financialPortfolioId,
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            json: true
+        };
+
+        const req = http.request(options, (res) => {
+          resolve('Success');
+        });
+
+        req.on('error', (e) => {
+          reject(e.message);
+        });
+
+        // send the request
+        req.write('');
+        req.end();
+    });
+}
+
+function deleteAllAccount(event) {
+    
+    return new Promise((resolve, reject) => {
+        const options = {
+            host: 'REPLACE HOST INSTANCE',
+            path: '/api/bankaccount/?financialPortfolioId=' + event['body-json'].financialPortfolioId,
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            json: true
+        };
+
+        const req = http.request(options, (res) => {
+          resolve('Success');
+        });
+
+        req.on('error', (e) => {
+          reject(e.message);
+        });
+
+        // send the request
+        req.write('');
+        req.end();
+    });
 }
