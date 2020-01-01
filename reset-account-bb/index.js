a5992526168ba99376ac1ac5042d5ef54ca7d18d
Arjun <nagarjun_nagesh@outlook.com>
2020-01-01 18:34:50 +0100

    update currency & reset Account BB with proper JWt token validation of body data

--- reset-account-bb/index.js
+++ reset-account-bb/index.js
@@ -1,20 +1,71 @@
 const http = require('http');
-exports.handler =  async function(event, context) {
-   // Concurrently call multiple APIs and wait for the response
+const AWS = require('aws-sdk')
+AWS.config.update({region: 'eu-west-1'});
+let lambda = new AWS.Lambda();
+let params = {
+    FunctionName: 'retrieveClaimsWithAuthHead', // the lambda function we are going to invoke
+    InvocationType: 'RequestResponse',
+    LogType: 'Tail'
+};
+let errorRespUA = "Mismatch of Financial Portfolio Id to reset the account";
+
+exports.handler =  async function(event) {
+   let invLamRes = '';
+   // Waits for the first call to be satisfied
+   await invokeLambda(event).then(function(result) {
+       console.log("The financial portfolio id matches.");
+       invLamRes = result;
+    }, function(err) {
+        console.log(err);
+        return err;
+    });
+    
+    if(invLamRes == '') {
+        throw new Error(errorRespUA);
+    }
+   
+   // Concurrently call multiple APIs and wait for the response 
    let events = [];
    events.push(deleteAllTransactions(event));
    events.push(deleteAllBudget(event));
    events.push(deleteAllAccount(event));
    let result = await Promise.all(events);
    console.log('The reset account for ' + event.params.querystring.financialPortfolioId + ' was ' + JSON.stringify(result));
+    
    return Object.assign({ result });
 }
 
+// Invoke Lambda retrieveClaim function
+function invokeLambda(event) {
+    // Authorization 
+      params.Payload = JSON.stringify({ "Authorization" :  event.params.header.Authorization});
+    
+       return new Promise((resolve, reject) => {
+           lambda.invoke(params, function(err, data) {
+               let payLoad = JSON.parse(data.Payload);
+                if (err || payLoad.status == '401') {
+                  console.log("Error occurred while authenticating JWT Token - " + data.Payload);
+                  reject(err);
+                } else {
+                  let ftpFromJWTToken = payLoad['custom:financialPortfolioId'];
+                  let userFPI = event.params.querystring.financialPortfolioId;
+                  if(ftpFromJWTToken == userFPI) {
+                      resolve({ "financialPortfolioId" : ftpFromJWTToken});
+                  } else {
+                      console.log(" Entered financialPortfolioId - " + userFPI + ' is not equal to JWT Token financialPortfolioId - ' + ftpFromJWTToken + ' ');
+                      reject("financialPortfolioId mismatch");
+                  }
+                }
+            });
+       }); 
+}
+
+// Delete all transactions from an API call to EC2 function
 function deleteAllTransactions(event) {
 
     return new Promise((resolve, reject) => {
         const options = {
-            host: 'ec2-****************.compute.amazonaws.com',
+            host: 'ec2-54-229-168-165.eu-west-1.compute.amazonaws.com',
             path: '/api/transactions/' + event.params.querystring.financialPortfolioId,
             method: 'DELETE',
             headers: {
@@ -37,11 +88,12 @@ function deleteAllTransactions(event) {
     });
 };
 
+// Delete all budget from an API call to EC2 function
 function deleteAllBudget(event) {
     
     return new Promise((resolve, reject) => {
         const options = {
-            host: 'ec2-****************.compute.amazonaws.com',
+            host: 'ec2-54-229-168-165.eu-west-1.compute.amazonaws.com',
             path: '/api/budget/?financialPortfolioId=' + event.params.querystring.financialPortfolioId,
             method: 'DELETE',
             headers: {
@@ -64,11 +116,12 @@ function deleteAllBudget(event) {
     });
 }
 
+// Delete all account from an API call to EC2 function
 function deleteAllAccount(event) {
     
     return new Promise((resolve, reject) => {
         const options = {
-            host: 'ec2-****************.compute.amazonaws.com',
+            host: 'ec2-54-229-168-165.eu-west-1.compute.amazonaws.com',
             path: '/api/bankaccount/?financialPortfolioId=' + event.params.querystring.financialPortfolioId,
             method: 'DELETE',
             headers: {
--- update-currency/index.js
+++ update-currency/index.js
@@ -1,11 +1,72 @@
 const AWS = require('aws-sdk')
 AWS.config.update({region: 'eu-west-1'});
+let cognitoIdServiceProvider = new AWS.CognitoIdentityServiceProvider();
+let lambda = new AWS.Lambda();
+let params = {
+    FunctionName: 'retrieveClaimsWithAuthHead', // the lambda function we are going to invoke
+    InvocationType: 'RequestResponse',
+    LogType: 'Tail'
+};
+// Create the DynamoDB service object
+let ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-function getItem(cty, event, context) {
-        // Create the DynamoDB service object
-        var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+exports.handler = async function(event, context) {
+    
+    // Waits for the first call to be satisfied
+    let lamErr = '';
+    await invokeLambda(event).then(function(result) {
+       console.log("The Email ID matches.");
+       lamErr = result;
+    }, function(err) {
+       throw new Error("Error Authenticating JWT token and Matching the given Email ID's " + err);
+    });
+    
+    // If Email ID is populated then execute the update currency
+    if(lamErr != '' && lamErr.email) {
+        let currency = '';
+        await getItem(event).then(function(result) {
+           currency = result;
+        }, function(err) {
+           throw new Error("Currency information cannot be obtained " + err);
+        });
         
-        var ddbParams = {
+        // If Dynamo DB response is positive
+        if(currency != '' && currency.currency) {
+            await updateCogItem(currency.currency, event);
+        }
+    }
+    
+    return event;
+};
+
+// Invoke Lambda retrieveClaim function
+function invokeLambda(event) {
+    // Authorization 
+      params.Payload = JSON.stringify({ "Authorization" :  event.params.header.Authorization});
+    
+      return new Promise((resolve, reject) => {
+           lambda.invoke(params, function(err, data) {
+               let payLoad = JSON.parse(data.Payload);
+                if (err || payLoad.status == '401') {
+                  console.log("Error occurred while authenticating JWT Token - " + data.Payload);
+                  reject(err);
+                } else {
+                   let emailFromJWTToken = JSON.parse(data.Payload).email;
+                   if(emailFromJWTToken == event['body-json'].userName) {
+                      resolve({ "email" : emailFromJWTToken});
+                   } else {
+                      console.log(" Entered Email address - " + event['body-json'].userName + ' is not equal to JWT Token Email - ' + emailFromJWTToken);
+                      reject("email address mismatch");
+                   }
+                }
+            });
+       }); 
+}
+
+function getItem(event, context) {
+        let cty = event.params.header['CloudFront-Viewer-Country'];
+        
+        let ddbParams = {
           AttributesToGet: [
              "currency"
           ],
@@ -18,18 +79,20 @@ function getItem(cty, event, context) {
         };
         
         // Call DynamoDB to read the item from the table
-        ddb.getItem(ddbParams, function(err, data) {
-          if (err) {
-            console.log("Error ", err);
-            context.done(err, event);
-          } else {
-            console.log("Success ", data.Item.currency.S);
-            return updateCogItem(data.Item.currency.S, event, context);
-          }
+        return new Promise((resolve, reject) => {
+            ddb.getItem(ddbParams, function(err, data) {
+              if (err) {
+                console.log("Error ", err);
+                reject(err);
+              } else {
+                console.log("Success ", data.Item.currency.S);
+                resolve({ "currency" : data.Item.currency.S});
+              }
+            });
         });
 }
 
-function updateCogItem(currency, event, context) {
+function updateCogItem(currency, event) {
        let accepLan = JSON.stringify(event.params.header['Accept-Language']);
         const params = {
             UserAttributes: [
@@ -45,18 +108,15 @@ function updateCogItem(currency, event, context) {
             UserPoolId: 'eu-west-1_cjfC8qNiB',
             Username: event['body-json'].userName,
         };
-        var cognitoIdServiceProvider = new AWS.CognitoIdentityServiceProvider();
-        cognitoIdServiceProvider.adminUpdateUserAttributes(params, function(err, data) {
-            if (err) context.done(err, event); // an error occurred
-            else {
-                event['body-json'].currency = currency; // Add currency to the reponse event
-                event['body-json'].locale = accepLan.substring(1,6); // Add locale to the response event
-                context.done(null, event); // successful response
-            }
+        
+        return new Promise((resolve, reject) => {
+            cognitoIdServiceProvider.adminUpdateUserAttributes(params, function(err, data) {
+                if (err) reject(err); // an error occurred
+                else {
+                    event['body-json'].currency = currency; // Add currency to the reponse event
+                    event['body-json'].locale = accepLan.substring(1,6); // Add locale to the response event
+                    resolve(event); // successful response
+                }
+            });
         });
 }
-
-exports.handler = function async(event, context) {
-    
-        return getItem(event.params.header['CloudFront-Viewer-Country'], event, context);
-};
