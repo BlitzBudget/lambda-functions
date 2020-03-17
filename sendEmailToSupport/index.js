decaf558a0db0d809568c14591b046a3c81bdfee
Arjun <nagarjun_nagesh@outlook.com>
2020-03-17 21:57:28 +0100

    async await send email

--- sendEmailToSupport/index.js
+++ sendEmailToSupport/index.js
@@ -4,7 +4,7 @@
 var aws = require('aws-sdk');
 var ses = new aws.SES({region: 'eu-west-1'});
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event) => {
     
      var params = {
         Destination: {
@@ -12,29 +12,18 @@ exports.handler = (event, context, callback) => {
         },
         Message: {
             Body: {
-                Text: { Data: event['body-json'].message
-                    
+                Text: { 
+                    Data: event['body-json'].message
                 }
                 
             },
-            
-            Subject: { Data: "Customer Support: Requesting More Information"
-                
+            Subject: { 
+                Data: "Customer Support: Requesting More Information"
             }
         },
         Source: event['body-json'].email
     };
 
     
-     ses.sendEmail(params, function (err, data) {
-        callback(null, {err: err, data: data});
-        if (err) {
-            console.log(err);
-            context.fail(err);
-        } else {
-            
-            console.log(data);
-            context.succeed(event);
-        }
-    });
+     await ses.sendEmail(params).promise();
 };
