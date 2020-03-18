3b59241cd69cf4f5b422cf242ab275e1254554cd
Arjun <nagarjun_nagesh@outlook.com>
2020-03-18 16:22:05 +0100

    rewrite url for help

--- rewriteUrlForHelp/index.js
+++ rewriteUrlForHelp/index.js
@@ -7,13 +7,16 @@ const config = {
 	
 };
 
+const regexSuffixless = /\/[^/.]+$/; // e.g. "/some/page" but not "/", "/some/" or "/some.jpg"
+const regexTrailingSlash = /.+\/$/; // e.g. "/some/" or "/some/page/" but not root "/"
+
 exports.handler =  async (event, context) => {
 	const { request } = event.Records[0].cf;
 	const { uri } = request;
 	const { suffix } = config;
 
-	// make "index.html" as origin request
-	if (suffix) {
+	// make "index.html" as origin request if the endpoints has a trialing slash or is suffix less
+	if (suffix && (uri.match(regexTrailingSlash) || uri.match(regexSuffixless))) {
 	    request.uri = suffix;
 	}
 
