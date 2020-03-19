bbf5a031e1fecfde1b568d32caf7d31199d2b452
Arjun <nagarjun_nagesh@outlook.com>
2020-03-19 13:10:26 +0100

    rewriteUrlForHelp

--- rewriteUrlForHelp/index.js
+++ rewriteUrlForHelp/index.js
@@ -3,21 +3,34 @@
  */
 
 const config = {
-	suffix: '/index.html',
-	
+	suffix: '/index.html'
 };
 
 const regexSuffixless = /\/[^/.]+$/; // e.g. "/some/page" but not "/", "/some/" or "/some.jpg"
 const regexTrailingSlash = /.+\/$/; // e.g. "/some/" or "/some/page/" but not root "/"
+const imgPath = '/img/';
+const jsPath = '/js/';
+const cssPath = '/css/';
 
 exports.handler =  async (event, context) => {
 	const { request } = event.Records[0].cf;
 	const { uri } = request;
 	const { suffix } = config;
 
-	// make "index.html" as origin request if the endpoints has a trialing slash or is suffix less
+	// make "index.html" as origin request if the endpoints has a trialing slash or is suffix
 	if (suffix && (uri.match(regexTrailingSlash) || uri.match(regexSuffixless))) {
 	    request.uri = suffix;
+	}
+	
+	if(uri.includes(imgPath)) {
+		let uriArray = uri.split(imgPath);
+		request.uri = imgPath + uriArray[1];
+	} else if (uri.includes(jsPath)) {
+		let uriArray = uri.split(jsPath);
+		request.uri = jsPath + uriArray[1];
+	} else if(uri.includes(cssPath)) {
+		let uriArray = uri.split(cssPath);
+		request.uri = cssPath + uriArray[1];
 	}
 
 	// If nothing matches, return request unchanged
