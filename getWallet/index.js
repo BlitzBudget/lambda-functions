c8234c26893444f68d7a57d1c8dc6f3ba98dc0b7
Arjun <nagarjun_nagesh@outlook.com>
2020-04-09 12:39:14 +0200

    get wallet

--- getWallet/index.js
+++ getWallet/index.js
@@ -11,8 +11,8 @@ exports.handler = async (event) => {
   let walletData = '';
   
     await getWalletItem(event.params.querystring.financialPortfolioId).then(function(result) {
-       console.log("successfully fetched the new wallet ");
-       walletData = result.success.Item.wallets.SS;
+       console.log("successfully fetched the new wallet " + JSON.stringify(result));
+       walletData = isEmpty(result.success) ? result.success : result.success.Item.wallets.SS;
     }, function(err) {
        throw new Error("Unable to fetch the wallet " + err);
     });
@@ -41,4 +41,22 @@ function getWalletItem(financialPortfolioId) {
           }
         });
     });
+}
+
+function  isEmpty(obj) {
+  // Check if objext is a number or a boolean
+  if(typeof(obj) == 'number' || typeof(obj) == 'boolean') return false; 
+  
+  // Check if obj is null or undefined
+  if (obj == null || obj === undefined) return true;
+  
+  // Check if the length of the obj is defined
+  if(typeof(obj.length) != 'undefined') return obj.length == 0;
+   
+  // check if obj is a custom obj
+  for(let key in obj) {
+        if(obj.hasOwnProperty(key))return false;
+  }
+      
+  return true;
 }
