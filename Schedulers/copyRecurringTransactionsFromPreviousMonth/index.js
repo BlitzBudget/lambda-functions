35e7b8e7507fb08e2836e8095c4d0cf6d63f6adf
C0625509 <nagarjun_nagesh@carrefour.com>
2020-05-01 14:05:19 +0200

    using scan to retrieve month data

--- Schedulers/copyRecurringTransactionsFromPreviousMonth/index.js
+++ Schedulers/copyRecurringTransactionsFromPreviousMonth/index.js
@@ -1,4 +1,31 @@
-const http = require('http');
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "eu-west-1"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+let today = new Date();
+let transactionSK = 'Transaction#' + today.getFullYear() + '-' + ('0' + today.getMonth()).slice(-2);
+
+var params = {
+    TableName: "blitzbudget",
+    ProjectionExpression: "#pk, #sk, amount, description, category, recurrence",
+    FilterExpression: "begins_with(#pk, :starts_with) and begins_with(#sk, :sk_starts_with) and #r = :re",
+    ExpressionAttributeNames: {
+        "#pk": "pk",
+        "#sk": "sk",
+        "#r": "recurrence",
+    },
+    ExpressionAttributeValues: {
+         ":starts_with": 'Wallet#',
+         ":sk_starts_with": transactionSK,
+         ":re": 'MONTHLY'
+    }
+};
+
+
 exports.handler = async (event) => {
     return await copyRecurringTransactionsFromPreviousMonths();
 };
@@ -6,28 +33,32 @@ exports.handler = async (event) => {
 
 // Delete all transactions from an API call to EC2 function
 function copyRecurringTransactionsFromPreviousMonths() {
+    console.log('Searching all transactions with sk %j', transactionSK, " with previous date as %k", today.toISOString());
     
     return new Promise((resolve, reject) => {
-        const options = {
-            host: 'ec2-54-229-168-165.eu-west-1.compute.amazonaws.com',
-            path: '/api/transactions/copyFromPreviousMonth',
-            method: 'POST',
-            headers: {
-              "Content-Type": "application/x-www-form-urlencoded",
-            },
-            json: true
-        };
-
-        const req = http.request(options, (res) => {
-          resolve('transactions copied from previous month');
-        });
-
-        req.on('error', (e) => {
-          reject(e.message);
-        });
-
-        // send the request
-        req.write('');
-        req.end();
+        console.log("Scanning Transactions table.");
+        docClient.scan(params, onScan);
     });
+}
+
+function onScan(err, data) {
+    if (err) {
+        console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        // print all the movies
+        console.log("Scan succeeded.");
+        data.Items.forEach(function(transaction) {
+           console.log(
+                transaction.sk + ": ",
+                transaction.pk, "- recurrence:", transaction.recurrence);
+        });
+
+        // continue scanning if we have more movies, because
+        // scan can retrieve a maximum of 1MB of data
+        if (typeof data.LastEvaluatedKey != "undefined") {
+            console.log("Scanning for more...");
+            params.ExclusiveStartKey = data.LastEvaluatedKey;
+            docClient.scan(params, onScan);
+        }
+    }
 }
