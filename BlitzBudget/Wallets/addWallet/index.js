43523f48d6bf3d949d1e67130159cf2ea01262a2
C0625509 <nagarjun_nagesh@carrefour.com>
2020-05-02 19:17:10 +0200

    add wallet

--- BlitzBudget/Wallets/addWallet/index.js
+++ BlitzBudget/Wallets/addWallet/index.js
@@ -7,8 +7,10 @@ AWS.config.update({region: 'eu-west-1'});
 var DB = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    
-    await addNewWallet(event).then(function(result) {
+    let walletId = isEmpty(event.Records) ? event['body-json'].walletId : event.Records[0].Sns.Message;
+    let currency = isEmpty(event.Records) ? event['body-json'].currency : event.Records[0].Sns.MessageAttributes.currency.Value;
+    console.log("events ", JSON.stringify(event));
+    await addNewWallet(event, walletId, currency).then(function(result) {
        console.log("successfully saved the new wallet");
     }, function(err) {
        throw new Error("Unable to add the wallet " + err);
@@ -17,16 +19,16 @@ exports.handler = async (event) => {
     return event;
 };
 
-function addNewWallet(event) {
+function addNewWallet(event, walletId, currency) {
     let today = new Date();
     let randomValue = "Wallet#" + today.toISOString(); 
         
     var params = {
       TableName:'blitzbudget',
       Item:{
-            "pk": event['body-json'].walletId,
+            "pk": walletId,
             "sk": randomValue,
-            "currency": event['body-json'].currency,
+            "currency": currency,
             "wallet_balance": 0,
             "total_asset_balance": 0,
             "total_debt_balance": 0,
@@ -43,9 +45,34 @@ function addNewWallet(event) {
             reject(err);
           } else {
             resolve({ "success" : data});
-            event['body-json'].id= randomValue;
+            if(isNotEmpty(event['body-json'])) {
+              event['body-json'].id= randomValue; 
+            }
           }
       });
     });
     
+}
+
+
+function  isEmpty(obj) {
+  // Check if objext is a number or a boolean
+  if(typeof(obj) == 'number' || typeof(obj) == 'boolean') return false; 
+  
+  // Check if obj is null or undefined
+  if (obj == null || obj === undefined) return true;
+  
+  // Check if the length of the obj is defined
+  if(typeof(obj.length) != 'undefined') return obj.length == 0;
+   
+  // check if obj is a custom obj
+  for(let key in obj) {
+        if(obj.hasOwnProperty(key))return false;
+    }
+
+  return true;
+}
+
+function isNotEmpty(obj) {
+  return !isEmpty(obj);
 }
