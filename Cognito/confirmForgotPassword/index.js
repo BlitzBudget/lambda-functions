e838abb875ec1a4abeb5766d5f587278953fc47d
C0625509 <nagarjun_nagesh@carrefour.com>
2020-05-02 19:23:25 +0200

    forgot password

--- Cognito/confirmForgotPassword/index.js
+++ Cognito/confirmForgotPassword/index.js
@@ -31,10 +31,37 @@ exports.handler = async (event) => {
     }, function(err) {
        throw new Error("Unable to login from cognito  " + err);
     });
+
+    await getUser(response).then(function(result) {
+       response.Username = result.Username;
+       response.UserAttributes = result.UserAttributes;
+       console.log("logged in the user " + JSON.stringify(result.Username));
+    }, function(err) {
+       throw new Error("Unable to get user attributes from cognito  " + err);
+    });
     
     
     return response;
 };
+
+function getUser(response) {
+    let params = {
+      AccessToken: response.AuthenticationResult.AccessToken /* required */
+    };
+
+    return new Promise((resolve, reject) => {
+        cognitoidentityserviceprovider.getUser(params, function(err, data) {
+          if (err) {
+              console.log(err, err.stack); // an error occurred
+              reject(err);
+          }
+          else {
+              console.log(data);           // successful response
+              resolve(data);
+          }
+        });
+    });
+}
 
 function confirmForgotPassword(params) {
     return new Promise((resolve, reject) => {
