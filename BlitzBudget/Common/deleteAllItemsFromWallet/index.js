b0587f4a635b39190a47ded18cdad957aba2dadc
C0625509 <nagarjun_nagesh@carrefour.com>
2020-05-06 10:40:57 +0200

    delete all items from wallet ptroper

--- BlitzBudget/Common/deleteAllItemsFromWallet/index.js
+++ BlitzBudget/Common/deleteAllItemsFromWallet/index.js
@@ -1,90 +1,109 @@
-const http = require('http');
-const AWS = require('aws-sdk')
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
 AWS.config.update({region: 'eu-west-1'});
-// Delete User
-let cognitoIdServiceProvider = new AWS.CognitoIdentityServiceProvider();
-const userPoolId = 'eu-west-1_cjfC8qNiB';
-let paramsDelete = {
-    UserPoolId: userPoolId, /* required */
+
+// Create the DynamoDB service object
+var DB = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+    console.log( 'event ' + JSON.stringify(event.Records[0]));
+    let walletId = event.Records[0].Sns.Message;
+    let result = {};
+    
+    await getAllItems(walletId).then(function(res) {
+       console.log("successfully fetched all the items ", res);
+       result = res;
+    }, function(err) {
+       throw new Error("Unable to delete the goals " + err);
+    });
+    
+    if(result.Count == 0) {
+        console.log("There are no items to delete for the wallet %j", walletId);
+        return event;
+    }
+    
+    console.log("Starting to process the batch delete request for the item for the wallet %j", result.Count);
+    let params = {};
+    params.RequestItems = {};
+    params.RequestItems.blitzbudget = [];
+    let processBatchDelete;
+    for(let i = 0, len = result.Items.length; i < len; i++) {
+        processBatchDelete = false;
+        let item = result.Items[i];
+        
+        if(params.RequestItems.blitzbudget.length < 25) {
+            console.log("Building the delete params for the item %j", item.sk);
+             params.RequestItems.blitzbudget[i] = { 
+                    "DeleteRequest": { 
+                       "Key": {
+                           "pk": walletId,
+                           "sk": item.sk
+                       }
+                    }
+                 };
+                 
+                 /*
+                 * If last iteration is running then
+                 */
+                 if(i == (len -1)) {
+                     processBatchDelete = true;
+                 }
+        } else {
+           processBatchDelete = true;
+        }
+        
+        if(processBatchDelete) {
+            console.log("Deleting a batch of %j items", params.RequestItems.blitzbudget.length);
+            await deleteItems(params).then(function(result) {
+               console.log("successfully deleted all the items");
+                params.RequestItems.blitzbudget = [];
+            }, function(err) {
+               throw new Error("Unable to delete all the items " + err);
+            });   
+        }
+    }
+        
+    return event;
 };
 
-var sns = new AWS.SNS();
-
-exports.handler =  async function(event) {
-   
-   // Concurrently call multiple APIs and wait for the response 
-   let events = [];
-   
-   if(event.params.querystring.deleteAccount == 'true') {
-        events.push(deleteCognitoAccount(event));   
-   }
-
-   events.push(resetAccountSubscriberThroughSNS(event));   
-   let result = await Promise.all(events);
-   console.log('The reset account for ' + event.params.querystring.financialPortfolioId + ' was ' + JSON.stringify(result));
+// Get goal Item
+function getAllItems(walletId) {
+    var params = {
+      TableName: 'blitzbudget',
+      KeyConditionExpression   : "pk = :walletId",
+      ExpressionAttributeValues: {
+          ":walletId": walletId
+      },
+      ProjectionExpression: "sk"
+    };
     
-   return Object.assign({ result });
-}
-
-// Delete Cognito Account
-function deleteCognitoAccount(event) {
-    paramsDelete.Username = event.params.querystring.userName;
-    
+    // Call DynamoDB to read the item from the table
     return new Promise((resolve, reject) => {
-        cognitoIdServiceProvider.adminDeleteUser(paramsDelete, function(err, data) {
-            if (err) reject(err); // an error occurred
-            else     resolve('Delete Account Success');           // successful response
+        DB.query(params, function(err, data) {
+          if (err) {
+            console.log("Error ", err);
+            reject(err);
+          } else {
+            console.log("data retrieved ", JSON.stringify(data.Items));
+            resolve(data);
+          }
         });
     });
 }
 
-function resetAccountSubscriberThroughSNS(event) {
-    console.log("Publishing to ResetAccountListener SNS or wallet id - " + event.params.querystring.financialPortfolioId);
-    let financialPortfolioId = isNotEmpty(event.params.querystring.referenceNumber) ? event.params.querystring.referenceNumber : event.params.querystring.financialPortfolioId;
-    let deleteOneWalletAttribute = isNotEmpty(event.params.querystring.referenceNumber) ? "execute" : "donotexecute";
-    
-    var params = {
-        Message: financialPortfolioId,
-        Subject: event.params.querystring.financialPortfolioId,
-        MessageAttributes: {
-            "delete_one_wallet": {
-                "DataType": "String",
-                "StringValue": deleteOneWalletAttribute
-            }
-        },
-        TopicArn: 'arn:aws:sns:eu-west-1:064559090307:ResetAccountSubscriber'
-    };
-    
-    return new Promise((resolve, reject) => {
-        sns.publish(params,  function(err, data) {
-            if (err) {
-                console.log("Error ", err);
-                reject(err);
-            } else {
-                resolve( "Reset account to SNS published");
-            }
-        }); 
-    });
-}
 
-function  isEmpty(obj) {
-  // Check if objext is a number or a boolean
-  if(typeof(obj) == 'number' || typeof(obj) == 'boolean') return false; 
-  
-  // Check if obj is null or undefined
-  if (obj == null || obj === undefined) return true;
-  
-  // Check if the length of the obj is defined
-  if(typeof(obj.length) != 'undefined') return obj.length == 0;
+function deleteItems(params) {
    
-  // check if obj is a custom obj
-  for(let key in obj) {
-        if(obj.hasOwnProperty(key))return false;
-    }
-
-  return true;
-}
-
-function isNotEmpty(obj) {
-    return !isEmpty(obj)
+    return new Promise((resolve, reject) => {
+        DB.batchWrite(params, function(err, data) {
+          if (err) {
+            console.log("Error ", err);
+            reject(err);
+          } else {
+              console.log("All items are successfully deleted");
+            resolve({ "success" : data});
+          }
+        });
+    });
 }
