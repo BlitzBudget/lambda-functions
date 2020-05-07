46320d5831d38e116ae35f9762daf31b2024f9b9
C0625509 <nagarjun_nagesh@carrefour.com>
2020-05-07 12:03:37 +0200

    Delete user

---
+++ Cognito/deleteUser/index.js
@@ -0,0 +1,33 @@
+const AWS = require('aws-sdk')
+AWS.config.update({region: 'eu-west-1'});
+let cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
+
+exports.handler = async (event) => {
+    let response = {};
+    var params = {
+      AccessToken: event.params.header.Authorization /* required */
+    };
+    
+    await deleteUser(params).then(function(result) {
+       response = result;
+    }, function(err) {
+       throw new Error("Unable to signin from cognito  " + err);
+    });
+    
+    return response;
+};
+
+function deleteUser(params) {
+    return new Promise((resolve, reject) => {
+        cognitoidentityserviceprovider.deleteUser(params, function(err, data) {
+          if (err) {
+              console.log(err, err.stack); // an error occurred
+              reject(err);
+          }
+          else {
+              console.log(data);           // successful response
+              resolve(data);
+          }
+        });
+    });
+}
