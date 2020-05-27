85948b893fa04a2bf1d7d024267e3c4cae4865e0
C0625509 <nagarjun_nagesh@carrefour.com>
2020-05-27 15:59:30 +0200

    check for category before adding transactions

--- BlitzBudget/Transactions/addTransactions/index.js
+++ BlitzBudget/Transactions/addTransactions/index.js
@@ -40,14 +40,28 @@ exports.handler = async (event) => {
     * If category Id is not present
     */
     let categoryName = event['body-json'].category;
-    if(notIncludesStr(categoryName, 'Category#')) {
+    if(isNotEmpty(categoryName) && notIncludesStr(categoryName, 'Category#')) {
       let today = new Date();
       today.setYear(event['body-json'].dateMeantFor.substring(5, 9));
       today.setMonth(parseInt(event['body-json'].dateMeantFor.substring(10, 12)) -1);
       let categoryId = "Category#" + today.toISOString();
-      // Assign Category to create the transactions with the category ID
-      event['body-json'].category = categoryId; 
-      events.push(createCategoryItem(event, categoryId, categoryName));
+      
+      /*
+      * Check if category is present before adding them
+      */
+      await getCategoryData(categoryId, event, today).then(function(result) {
+        if(isNotEmpty(result.Category)) {
+          console.log("successfully assigned the existing category %j", result.Category.sk);
+          event['body-json'].category = result.Category.sk;
+        } else {
+          // Assign Category to create the transactions with the category ID
+          event['body-json'].category = categoryId;
+          event['body-json'].categoryName = categoryName;
+          events.push(createCategoryItem(event, categoryId, categoryName));
+        }
+      }, function(err) {
+         throw new Error("Unable to get the category " + err);
+      });
     }
     
     /*
@@ -65,6 +79,46 @@ exports.handler = async (event) => {
         
     return event;
 };
+
+function getCategoryData(categoryId, event, today) {
+  var params = {
+      TableName: 'blitzbudget',
+      KeyConditionExpression   : "pk = :pk AND begins_with(sk, :items)",
+      ExpressionAttributeValues: {
+          ":pk": event['body-json'].walletId,
+          ":items": "Category#" + today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2)
+      },
+      ProjectionExpression: "pk, sk, category_name, category_type"
+    };
+    
+    // Call DynamoDB to read the item from the table
+    return new Promise((resolve, reject) => {
+        docClient.query(params, function(err, data) {
+          if (err) {
+            console.log("Error ", err);
+            reject(err);
+          } else {
+            console.log("data retrieved - Category %j", data.Count);
+            let obj;
+            if(isNotEmpty(data.Items)) {
+              for(const categoryObj of data.Items) {
+                if(isEqual(categoryObj['category_type'],event['body-json'].categoryType) 
+                && isEqual(categoryObj['category_name'],event['body-json'].category)) {
+                    console.log("Found a match for the mentioned category %j", categoryObj.sk);
+                    obj = categoryObj;
+                }
+              }
+            } 
+            
+            if(isEmpty(obj)) {
+              console.log("No matching categories found");
+            }
+            
+            resolve({ "Category" : obj}); 
+          }
+        });
+    });
+}
 
 function getDateData(pk, today) {
   var params = {
@@ -279,4 +333,11 @@ function  isEmpty(obj) {
 
 function isNotEmpty(obj) {
   return !isEmpty(obj);
+}
+
+function isEqual(obj1,obj2){
+  if (JSON.stringify(obj1) === JSON.stringify(obj2)) {
+      return true;
+  }
+  return false;
 }
