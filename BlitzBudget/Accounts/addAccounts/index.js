3c9f90723c1da1fa9d27c0700396d7a983da08fd
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2020-07-26 15:44:21 +0200

    add account add sub type

--- BlitzBudget/Accounts/addAccounts/index.js
+++ BlitzBudget/Accounts/addAccounts/index.js
@@ -1,53 +1,58 @@
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 // Set the region 
-AWS.config.update({region: 'eu-west-1'});
+AWS.config.update({
+    region: 'eu-west-1'
+});
 
 // Create the DynamoDB service object
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
     console.log("adding BankAccounts for ", JSON.stringify(event['body-json']));
-    await addNewBankAccounts(event).then(function(result) {
-       console.log("successfully saved the new BankAccounts");
-    }, function(err) {
-       throw new Error("Unable to add the BankAccounts " + err);
+    await addNewBankAccounts(event).then(function (result) {
+        console.log("successfully saved the new BankAccounts");
+    }, function (err) {
+        throw new Error("Unable to add the BankAccounts " + err);
     });
-        
+
     return event;
 };
 
 function addNewBankAccounts(event) {
     let today = new Date();
-    let randomValue = "BankAccount#" + today.toISOString(); 
-        
+    let randomValue = "BankAccount#" + today.toISOString();
+
     var params = {
-      TableName:'blitzbudget',
-      Item:{
+        TableName: 'blitzbudget',
+        Item: {
             "pk": event['body-json'].walletId,
             "sk": randomValue,
             "account_type": event['body-json'].accountType,
             "bank_account_name": event['body-json'].bankAccountName,
             "linked": event['body-json'].linked,
             "account_balance": event['body-json'].accountBalance,
+            "account_sub_type": event['body-json'].accountSubType,
             "selected_account": event['body-json'].selectedAccount,
             "primary_wallet": event['body-json'].primaryWallet,
             "creation_date": new Date().toISOString(),
             "updated_date": new Date().toISOString()
-      }
+        }
     };
-    
+
     console.log("Adding a new item...");
     return new Promise((resolve, reject) => {
-      docClient.put(params, function(err, data) {
-          if (err) {
-            console.log("Error ", err);
-            reject(err);
-          } else {
-            resolve({ "success" : data});
-            event['body-json'].id= randomValue;
-          }
-      });
+        docClient.put(params, function (err, data) {
+            if (err) {
+                console.log("Error ", err);
+                reject(err);
+            } else {
+                resolve({
+                    "success": data
+                });
+                event['body-json'].id = randomValue;
+            }
+        });
     });
-    
+
 }
