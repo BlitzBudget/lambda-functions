0608065da0f3e73d2b7f8a638e84b8339dc5913a
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2020-08-21 23:38:32 +0200

    account type, bank account name, account balance and select account is empty

--- BlitzBudget/Accounts/addAccounts/index.js
+++ BlitzBudget/Accounts/addAccounts/index.js
@@ -10,6 +10,25 @@ var docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
     console.log("adding BankAccounts for ", JSON.stringify(event['body-json']));
+    let walletId = event['body-json'].primaryWallet;
+
+    /*
+     * If accountType, bankAccountName, accountBalance or selectedAccount is empty
+     */
+    if (isEmpty(event['body-json'].accountType)) {
+        console.log("The account type is mandatory for adding an account %j", walletId);
+        throw new Error("Unable to add the transaction as bank account type is mandatory");
+    } else if (isEmpty(event['body-json'].bankAccountName)) {
+        console.log("The bank account name is mandatory for adding an account %j", walletId);
+        throw new Error("Unable to add the transaction as bank account name is mandatory");
+    } else if (isEmpty(event['body-json'].accountBalance)) {
+        console.log("The account balance is mandatory for adding an account %j", walletId);
+        throw new Error("Unable to add the transaction as account balance is mandatory");
+    } else if (isEmpty(event['body-json'].selectedAccount)) {
+        console.log("The selected account is mandatory for adding an account %j", walletId);
+        throw new Error("Unable to add the transaction as selected account is mandatory");
+    }
+
     await addNewBankAccounts(event).then(function (result) {
         console.log("successfully saved the new BankAccounts");
     }, function (err) {
@@ -54,5 +73,22 @@ function addNewBankAccounts(event) {
             }
         });
     });
+}
 
+function isEmpty(obj) {
+    // Check if objext is a number or a boolean
+    if (typeof (obj) == 'number' || typeof (obj) == 'boolean') return false;
+
+    // Check if obj is null or undefined
+    if (obj == null || obj === undefined) return true;
+
+    // Check if the length of the obj is defined
+    if (typeof (obj.length) != 'undefined') return obj.length == 0;
+
+    // check if obj is a custom obj
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) return false;
+    }
+
+    return true;
 }
