de8f21ac2c9eb2c63f6ac0b31778b61889125e59
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2020-09-09 22:21:46 +0200

    Send Email to support

--- BlitzBudget/Profile/sendEmailToSupport/index.js
+++ BlitzBudget/Profile/sendEmailToSupport/index.js
@@ -2,28 +2,30 @@
 // SPDX-License-Identifier: Apache-2.0
 
 var aws = require('aws-sdk');
-var ses = new aws.SES({region: 'eu-west-1'});
+var ses = new aws.SES({
+    region: 'eu-west-1'
+});
 
 exports.handler = async (event) => {
-    
-     var params = {
+    console.log("The from email address is ", event['body-json'].email, " The subject is ", event['body-json'].subject);
+    var params = {
         Destination: {
             ToAddresses: ["admin@blitzbudget.com"]
         },
         Message: {
             Body: {
-                Text: { 
+                Text: {
                     Data: event['body-json'].message
                 }
-                
+
             },
-            Subject: { 
-                Data: event['body-json'].support
+            Subject: {
+                Data: event['body-json'].subject
             }
         },
         Source: event['body-json'].email
     };
 
-    
-     await ses.sendEmail(params).promise();
+
+    await ses.sendEmail(params).promise();
 };
