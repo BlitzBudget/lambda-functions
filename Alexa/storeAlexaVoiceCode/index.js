e66294d1824cff66b90b39f7cb75e08cde594fcd
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2020-10-05 19:16:29 +0200

    login, get user , store alexa voice code

--- Alexa/storeAlexaVoiceCode/index.js
+++ Alexa/storeAlexaVoiceCode/index.js
@@ -1,9 +1,14 @@
 const crypto = require('crypto');
 
-const AWS = require('aws-sdk')
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region
 AWS.config.update({
     region: 'eu-west-1'
 });
+
+// Create the DynamoDB service object
+var docClient = new AWS.DynamoDB.DocumentClient();
 let cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 
 
@@ -11,6 +16,7 @@ const clientId = '18he7k81dv7k6fccf7giiuo84r';
 const clientSecret = 'ar8dgqr8e2jma0ojrkq5cu13bntnb624132uboirg9v6m1h4ge5';
 
 exports.handler = async (event) => {
+    let response = {};
     let username = event['body-json'].username;
 
     // Authorization code
@@ -42,15 +48,159 @@ exports.handler = async (event) => {
 
     await adminInitiateAuth(params).then(function (result) {
         console.log("The result of the initiate Auth is", result);
-        event = result;
+        response = result;
     }, function (err) {
         throw new Error("Unable to signin from cognito  " + err);
+    });
+
+    /*
+     * Get User Attributes
+     */
+    await getUser(response).then(function (result) {
+        response.Username = result.Username;
+        response.UserAttributes = result.UserAttributes;
+        console.log("logged in the user " + JSON.stringify(result.Username));
+    }, function (err) {
+        throw new Error("Unable to signin from cognito  " + err);
+    });
+
+    /*
+     * Fetch user id from cognito attributes
+     */
+    let userId;
+    for (let i = 0, len = response.UserAttributes.length; i < len; i++) {
+        let userAttribute = response.UserAttributes[i];
+        if (isEqual(userAttribute.Name, 'custom:financialPortfolioId')) {
+            // User Id
+            userId = userAttribute.Value;
+        }
+    }
+
+    /*
+     * User Id cannot be found
+     */
+    if (isEmpty(userId)) {
+        throw new Error("Unable to find the user id when trying to retrieve user attributes from cognito");
+    }
+
+    /*
+     * Add a new voice code
+     */
+    let today = new Date();
+    let alexaId = "AlexaVoiceCode#" + today.toISOString();
+
+    /*
+     * Check if the new voice code is present
+     */
+    await getNewVoiceCode(userId).then(function (result) {
+        if (result.Count > 0) {
+            for (const alexaVoiceCode of result.Items) {
+                console.log("successfully assigned the old voice code id ", alexaVoiceCode.sk);
+                alexaId = alexaVoiceCode.sk;
+            }
+        }
+    }, function (err) {
+        throw new Error("Unable to get a new voice code " + err);
+    });
+
+    /*
+     * Add a new voice code
+     */
+    await addNewVoiceCode(event, userId, alexaId).then(function (result) {
+        console.log("successfully added a new voice code");
+    }, function (err) {
+        throw new Error("Unable to add a new voice code " + err);
     });
 
 
     return event;
 };
 
+/*
+ * Get New Voice Code
+ */
+function getNewVoiceCode(userId) {
+    var params = {
+        TableName: 'blitzbudget',
+        KeyConditionExpression: "pk = :userId and begins_with(sk, :items)",
+        ExpressionAttributeValues: {
+            ":userId": userId,
+            ":items": "AlexaVoiceCode#"
+        },
+        ProjectionExpression: "voice_code, sk, pk"
+    };
+
+    // Call DynamoDB to read the item from the table
+    return new Promise((resolve, reject) => {
+        docClient.query(params, function (err, data) {
+            if (err) {
+                console.log("Error ", err);
+                reject(err);
+            } else {
+                console.log("data retrieved ", JSON.stringify(data.Items));
+                resolve(data);
+            }
+        });
+    });
+}
+
+/*
+ * Get User
+ */
+function getUser(response) {
+    let params = {
+        AccessToken: response.AuthenticationResult.AccessToken /* required */
+    };
+
+    return new Promise((resolve, reject) => {
+        cognitoidentityserviceprovider.getUser(params, function (err, data) {
+            if (err) {
+                console.log(err, err.stack); // an error occurred
+                reject(err);
+            } else {
+                console.log(data); // successful response
+                resolve(data);
+            }
+        });
+    });
+}
+
+/*
+ * Voice Code
+ */
+function addNewVoiceCode(event, userId, alexaId) {
+    let today = new Date().toISOString();
+
+    var params = {
+        TableName: 'blitzbudget',
+        Item: {
+            "pk": userId,
+            "sk": alexaId,
+            "voice_code": event['body-json'].voiceCode,
+            "creation_date": today,
+            "updated_date": today
+        }
+    };
+
+    console.log("Adding a new item...");
+    return new Promise((resolve, reject) => {
+        docClient.put(params, function (err, data) {
+            if (err) {
+                console.log("Error ", err);
+                reject(err);
+            } else {
+                resolve({
+                    "success": data
+                });
+                event['body-json'].alexaVoiceCodeId = alexaId;
+            }
+        });
+    });
+}
+
+/*
+ * Admin initiate authentication
+ */
 function adminInitiateAuth(params) {
     return new Promise((resolve, reject) => {
         cognitoidentityserviceprovider.adminInitiateAuth(params, function (err, data) {
@@ -83,6 +233,9 @@ function isEmpty(obj) {
     return true;
 }
 
-function isNotEmpty(obj) {
-    return !isEmpty(obj);
+function isEqual(obj1, obj2) {
+    if (JSON.stringify(obj1) === JSON.stringify(obj2)) {
+        return true;
+    }
+    return false;
 }
