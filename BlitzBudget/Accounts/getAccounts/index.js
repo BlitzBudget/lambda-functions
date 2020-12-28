ad186dda2bb8c88d2f2d8cd96e1f440563b10b46
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2020-12-28 21:58:17 +0100

    account Id as wallet id

--- BlitzBudget/Accounts/getAccounts/index.js
+++ BlitzBudget/Accounts/getAccounts/index.js
@@ -1,20 +1,24 @@
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 // Set the region 
-AWS.config.update({region: 'eu-west-1'});
+AWS.config.update({
+    region: 'eu-west-1'
+});
 
 // Create the DynamoDB service object
-var docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-1'});
+var docClient = new AWS.DynamoDB.DocumentClient({
+    region: 'eu-west-1'
+});
 
 
 exports.handler = async (event) => {
-  console.log("fetching item for the walletId ", event.params.querystring.walletId);
-  let BankAccountData = [];
-  
-    await getBankAccountItem(event.params.querystring.walletId).then(function(result) {
-       BankAccountData = result;
-    }, function(err) {
-       throw new Error("Unable error occured while fetching the BankAccount " + err);
+    console.log("fetching item for the walletId ", event.params.querystring.walletId);
+    let BankAccountData = [];
+
+    await getBankAccountItem(event.params.querystring.walletId).then(function (result) {
+        BankAccountData = result;
+    }, function (err) {
+        throw new Error("Unable error occured while fetching the BankAccount " + err);
     });
 
     return BankAccountData;
@@ -24,25 +28,25 @@ exports.handler = async (event) => {
 // Get BankAccount Item
 function getBankAccountItem(walletId) {
     var params = {
-      TableName: 'blitzbudget',
-      KeyConditionExpression   : "pk = :walletId and begins_with(sk, :items)",
-      ExpressionAttributeValues: {
-          ":walletId": walletId,
-          ":items": "BankAccount#"
-      },
-      ProjectionExpression: "bank_account_name, linked, bank_account_number, account_balance, sk, pk, selected_account, number_of_times_selected, account_type"
+        TableName: 'blitzbudget',
+        KeyConditionExpression: "pk = :walletId and begins_with(sk, :items)",
+        ExpressionAttributeValues: {
+            ":walletId": walletId,
+            ":items": "BankAccount#"
+        },
+        ProjectionExpression: "bank_account_name, linked, bank_account_number, account_balance, sk, pk, selected_account, number_of_times_selected, account_type, account_sub_type"
     };
-    
+
     // Call DynamoDB to read the item from the table
     return new Promise((resolve, reject) => {
-        docClient.query(params, function(err, data) {
-          if (err) {
-            console.log("Error ", err);
-            reject(err);
-          } else {
-            console.log("data retrieved ", JSON.stringify(data.Items));
-            resolve(data);
-          }
+        docClient.query(params, function (err, data) {
+            if (err) {
+                console.log("Error ", err);
+                reject(err);
+            } else {
+                console.log("data retrieved ", JSON.stringify(data.Items));
+                resolve(data);
+            }
         });
     });
 }
--- BlitzBudget/Transactions/getTransactions/index.js
+++ BlitzBudget/Transactions/getTransactions/index.js
@@ -387,7 +387,7 @@ function getBankAccountData(pk) {
             ":pk": pk,
             ":items": "BankAccount#"
         },
-        ProjectionExpression: "bank_account_name, linked, bank_account_number, account_balance, sk, pk, selected_account, number_of_times_selected, account_type"
+        ProjectionExpression: "bank_account_name, linked, bank_account_number, account_balance, sk, pk, selected_account, number_of_times_selected, account_type,  account_sub_type"
     };
 
     // Call DynamoDB to read the item from the table
@@ -400,7 +400,7 @@ function getBankAccountData(pk) {
                 console.log("data retrieved - Bank Account %j", data.Count);
                 for (const accountObj of data.Items) {
                     accountObj.accountId = accountObj.sk;
-                    accountObj.userId = accountObj.pk;
+                    accountObj.walletId = accountObj.pk;
                     delete accountObj.sk;
                     delete accountObj.pk;
                 }
