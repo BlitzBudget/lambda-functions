82c6be9415b3653ee9ce4365647505f96623b847
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2021-03-16 14:37:44 +0100

    util test

--- Cognito/signin/_tests_ /tests/utils/util.test.js
+++ Cognito/signin/_tests_ /tests/utils/util.test.js
@@ -1,4 +1,6 @@
 const util = require('../../../utils/util');
+const mockUser = require('../../fixtures/response/get-user');
+const mockWalletResponse = require('../../fixtures/response/wallet');
 
 describe('includesStr', () => {
   test('With Data: Success', () => {
@@ -16,5 +18,45 @@ describe('includesStr', () => {
   test('Without Data: Success', () => {
     expect(util.includesStr(['en', 'es'], '')).toBe(false);
     expect(util.includesStr([], 'en')).toBe(false);
+  });
+});
+
+describe('fetchUserId', () => {
+  test('With Data: Success', () => {
+    const userId = util.fetchUserId(mockUser);
+    expect(userId).not.toBeUndefined();
+    expect(userId).toBe(mockUser.UserAttributes[2].Value);
+  });
+
+  test('With False Data: Success', () => {
+    const userId = util.fetchUserId({});
+    expect(userId).toBeUndefined();
+  });
+
+  test('Without Data: Success', () => {
+    const userId = util.fetchUserId({ UserAttributes: {} });
+    expect(userId).toBeUndefined();
+  });
+});
+
+describe('nameKeysAppropriately', () => {
+  test('With Data: Success', () => {
+    const response = util.nameKeysAppropriately(mockWalletResponse);
+    expect(response).not.toBeUndefined();
+    expect(response[0].walletId).not.toBeUndefined();
+    expect(response[0].userId).not.toBeUndefined();
+    expect(response[0].pk).toBeUndefined();
+    expect(response[0].sk).toBeUndefined();
+  });
+
+  test('With False Data: Success', () => {
+    const response = util.nameKeysAppropriately({});
+    expect(response).toBeUndefined();
+  });
+
+  test('Without Data: Success', () => {
+    const response = util.nameKeysAppropriately({ Items: [] });
+    expect(response).not.toBeUndefined();
+    expect(response.length).toBe(0);
   });
 });
