0c10e4d973b67ae62bb78a4a44524127ca22d030
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2022-05-24 12:15:53 +0200

    send wallet as pk and sk

--- DynamoDb-Stream/processBlitzBudgetTable/index.js
+++
@@ -1,9 +0,0 @@
-console.log('Loading function');
-
-const updateHelper = require('./utils/update-helper');
-
-exports.handler = async (event) => {
-  updateHelper.updateRelevantItems(event);
-
-  return `Successfully processed ${event.Records.length} records.`;
-};
